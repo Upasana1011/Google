510cebd4629b98fea5ea9ca5d237f42e5626191a
Upasana Kuar <upasanakuar44@gmail.com>
2022-02-21 21:02:48 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,14 +7,14 @@ import axios from "axios";
 
 export const Home = () => {
     const [text,setText]=useState("")
-    const [data,setData]=useState([])
+//     const [dt,setData]=useState([])
     
-    useEffect(()=>{
-        axios.get("http://localhost:3000/google")
-        .then((data)=>{
-            setData(data.data)
-        })
-    },[])
+//     useEffect(()=>{
+//         axios.get("http://localhost:3000/google")
+//         .then((data)=>{
+//             setData(data.data)
+//         })
+//     },[])
     return (
         <div>
         <img width="300px"src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/368px-Google_2015_logo.svg.png" alt="google"></img>
