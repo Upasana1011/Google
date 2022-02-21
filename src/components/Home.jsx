e08195634a610d18728727d24b3931795b6b4108
Upasana Kuar <upasanakuar44@gmail.com>
2022-02-21 20:45:22 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
-import "./styles.css"
-//import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import "./styles.css";
+import axios from "axios";
+import { Link } from 'react-router-dom';
 
 export const Home = () => {
+    const [text,setText]=useState("")
+    const [dt,setData]=useState([])
+    
+    useEffect(()=>{
+        axios.get("http://localhost:3000/google")
+        .then((data)=>{
+            setData(data.data)
+        })
+    },[])
     return (
         <div>
-            <br /><br />
         <img width="300px"src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/368px-Google_2015_logo.svg.png" alt="google"></img>
         <br />
-        <input className='search-box' placeholder="search google" type="text" />  
+        <input className='search-box' placeholder="search google" type="text" onChange={(e)=>{
+            setText(e.target.value)
+        }} /> 
+        <button onClick={()=>{
+            window.location.href=`/search/q=${text}`
+        }}>Search</button> 
         </div>
     );
 };
-
